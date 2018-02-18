e0782fd16de375799dd0fa13abee07ac00278b23
Amanda Lima <aman.ol@hotmail.com>
2018-02-18 11:03:51 -0300

    Remove atributo tipo de cliente

--- Reserva/src/br/com/reserva/Cliente.java
+++ Reserva/src/br/com/reserva/Cliente.java
@@ -2,18 +2,10 @@ package br.com.reserva;
 
 public class Cliente extends Taxa{
 	
-	private String tipo;
 	
 	public Cliente (double taxaSemana, double taxaFimSemana) {
 		super.setTaxaSemana(taxaSemana);
 		super.setTaxaFimSemana(taxaFimSemana);
 	}
-	
-	public String getTipo() {
-		return tipo;
-	}
 
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
 }
